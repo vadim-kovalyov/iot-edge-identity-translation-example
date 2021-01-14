f61a6dfef71c03293c4846fc3b91f5312aac91d3
Vadim Kovalyov <vadim.kovalyov@gmail.com>
2021-01-13 17:13:01 -0800

    Added module_client that handles token refresh

--- modules/ptm_python/app/main.py
+++ modules/ptm_python/app/main.py
@@ -5,11 +5,12 @@ import time
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 
-import mqtt_client
+import module_client
 
 # A map of sensors' MAC to device id.
 # All devices need to be pre-created in IoT Hub.
-mac_to_device_id = {"FE:36:EA:1E:62:AF": "sensor_1", "E4:F2:81:30:8D:52": "sensor_3"}
+mac_to_device_id = {"FE:36:EA:1E:62:AF": "sensor_1",
+                    "E4:F2:81:30:8D:52": "sensor_3"}
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -17,7 +18,13 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 def on_connect(client, userdata, flags, rc):
     """ The callback for when the client receives a CONNACK response from the server.
     """
-    logging.info("Upstream client connected with result code "+str(rc))
+    logging.info(f"Upstream client connected with result code {str(rc)}")
+
+    # code 4 means bad username or password
+    # which in our case means expired token.
+    if rc == 4:
+        logging.info(f"Trying to refresh the SAS token.")
+        client.refresh()
 
 
 def publish_upstream(client, mac, payload):
@@ -38,7 +45,7 @@ def main():
     gateway_hostname = os.environ["IOTEDGE_GATEWAYHOSTNAME"]
 
     # create a client from environment variables.
-    client = mqtt_client.create_from_environment()
+    client = module_client.create_from_environment()
     client.on_connect = on_connect
     client.connect(gateway_hostname, port=8883)
 
---
+++ modules/ptm_python/app/module_client.py
@@ -0,0 +1,112 @@
+"""This module contains a helper methods to create paho.mqtt.client from
+edge module environmet.
+"""
+
+import logging
+import os
+import ssl
+
+import paho.mqtt.client as mqtt
+from azure.iot.device.common.auth import sastoken as auth
+from azure.iot.device.iothub import edge_hsm
+
+
+class ModuleClient(object):
+
+    def __init__(self, hostname, device_id, module_id, module_generation_id, workload_uri, api_version, sastoken_ttl):
+        self._username = f"{hostname}/{device_id}/{module_id}/?api-version={api_version}"
+
+        # Use an HSM for authentication in the general case
+        hsm = edge_hsm.IoTEdgeHsm(
+            module_id=module_id,
+            generation_id=module_generation_id,
+            workload_uri=workload_uri,
+            api_version=api_version,
+        )
+
+        # Create SasToken
+        uri = _form_sas_uri(hostname=hostname,
+                            device_id=device_id, module_id=module_id)
+        try:
+            self._token = auth.RenewableSasToken(
+                uri, hsm, ttl=sastoken_ttl)
+        except auth.SasTokenError as e:
+            raise ValueError(
+                "Could not create a SasToken using the values provided, or in the Edge environment"
+            ) from e
+
+        # Create TLS context
+        try:
+            server_verification_cert = hsm.get_certificate()
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+            context.load_verify_locations(cadata=server_verification_cert)
+        except edge_hsm.IoTEdgeError as e:
+            raise OSError(
+                "Unexpected failure getting server verification certificate"
+            ) from e
+
+        # Create mqtt client
+        self._mqtt_client = mqtt.Client(client_id=f"{device_id}/{module_id}")
+        self._mqtt_client.username_pw_set(self._username, str(self._token))
+        self._mqtt_client.tls_set_context(context)
+
+    def refresh(self):
+        self._token.refresh()
+        self._mqtt_client.username_pw_set(self._username, str(self._token))
+
+    def connect(self, host, port=1883, keepalive=60):
+        """Connect to a remote broker.
+        """
+        self._mqtt_client.connect(host, port, keepalive)
+
+    def publish(self, topic, payload=None, qos=0, retain=False, properties=None):
+        """Publish a message on a topic.
+        """
+        self._mqtt_client.publish(topic, payload, qos, retain, properties)
+
+    @property
+    def on_connect(self):
+        """If implemented, called when the broker responds to our connection
+        request."""
+        return self._mqtt_client.on_connect
+
+    @on_connect.setter
+    def on_connect(self, func):
+        """ Define the connect callback implementation. See paho.mqtt.client.on_connect
+        """
+
+        # wrap on_connect to replace mqtt_client with module_client.
+        def callback(client, userdata, flags, rc):
+            func(self, userdata, flags, rc)
+
+        self._mqtt_client.on_connect = callback
+
+    def loop_forever(self):
+        """This function call loop_forever() on inner mqtt client. See paho.mqtt.client.loop_forever
+        """
+        self._mqtt_client.loop_forever()
+
+
+def create_from_environment(sastoken_ttl=3600):
+    """Creates a paho.mqtt.client from edge module environmet. The returned object
+    has proper authentication context (username and password) already set.
+
+    :param int sastoken_ttl: The time to live (in seconds) for the created SasToken used for
+        authentication. Default is 3600 seconds (1 hour)
+    """
+
+    # Get the Edge container variables
+    hostname = os.environ["IOTEDGE_IOTHUBHOSTNAME"]
+    device_id = os.environ["IOTEDGE_DEVICEID"]
+    module_id = os.environ["IOTEDGE_MODULEID"]
+    module_generation_id = os.environ["IOTEDGE_MODULEGENERATIONID"]
+    workload_uri = os.environ["IOTEDGE_WORKLOADURI"]
+    api_version = os.environ["IOTEDGE_APIVERSION"]
+
+    client = ModuleClient(hostname, device_id, module_id,
+                          module_generation_id, workload_uri, api_version, sastoken_ttl)
+    return client
+
+
+def _form_sas_uri(hostname, device_id, module_id):
+    return f"{hostname}/devices/{device_id}/modules/{module_id}"
