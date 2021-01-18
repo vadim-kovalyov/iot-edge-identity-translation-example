e726d96403031a8a212d28a80caae532ba9cd0c0
Emmanuel Bertrand <ebertra@microsoft.com>
2021-01-18 16:59:31 +0100

    Only listening for signals from specific MACs

--- modules/ptm_python/app/main.py
+++ modules/ptm_python/app/main.py
@@ -9,8 +9,7 @@ import module_client
 
 # A map of sensors' MAC to device id.
 # All devices need to be pre-created in IoT Hub.
-mac_to_device_id = {"FE:36:EA:1E:62:AF": "sensor_1",
-                    "E4:F2:81:30:8D:52": "sensor_3"}
+authorized_devices = [('FE:36:EA:1E:62:AF', 'sensor_1'), ('E4:F2:81:30:8D:52', 'sensor_3')]
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
@@ -30,13 +29,15 @@ def on_connect(client, userdata, flags, rc):
 def publish_upstream(client, mac, payload):
     """ The callback for when a message is received from a sensor.
     """
-    if mac in mac_to_device_id:
-        device_id = mac_to_device_id[mac]
-        logging.info(f"publishing message for {device_id} ({mac}).")
-        client.publish(
-            f"$iothub/{device_id}/messages/events", str(payload), qos=1)
-    else:
-        logging.warning("ignoring message from an unknown sensor.")
+    #This loop to check for authorized devices should not be needed given that it should already be enforced by the ruuvitag library
+    is_authorized_device = False
+    for sensor in authorized_devices:
+        if mac == sensor[0]:
+            is_authorized_device = True
+            logging.info(f"publishing message for {sensor[1]} ({mac}).")
+            client.publish(f"$iothub/{sensor[1]}/messages/events", str(payload), qos=1)
+    if(is_authorized_device == False):
+        logging.warning(f"ignoring message from an unknown sensor (MAC address: {mac}).")
 
 
 def main():
@@ -58,7 +59,7 @@ def main():
 
     # listening for sensors data.
     logging.info("listening for sensors data.")
-    RuuviTagSensor.get_datas(callback)
+    RuuviTagSensor.get_datas(callback, [sensor[0] for sensor in authorized_devices])
 
     logging.info("exiting.")
 
