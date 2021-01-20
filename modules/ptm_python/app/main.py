0da8dfeeaa626b58a913623c416be2c06980c78f
Vadim Kovalyov <vadim.kovalyov@gmail.com>
2021-01-19 16:54:33 -0800

    Improved logging + dockerignore

--- modules/ptm_python/app/main.py
+++ modules/ptm_python/app/main.py
@@ -9,9 +9,13 @@ import module_client
 
 # A map of sensors' MAC to device id.
 # All devices need to be pre-created in IoT Hub.
-authorized_devices = [('FE:36:EA:1E:62:AF', 'sensor_1'), ('E4:F2:81:30:8D:52', 'sensor_3')]
+authorized_devices = [('FE:36:EA:1E:62:AF', 'sensor_1'),
+                      ('E4:F2:81:30:8D:52', 'sensor_3')]
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.DEBUG,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def on_connect(client, userdata, flags, rc):
@@ -29,19 +33,23 @@ def on_connect(client, userdata, flags, rc):
 def publish_upstream(client, mac, payload):
     """ The callback for when a message is received from a sensor.
     """
-    #This loop to check for authorized devices should not be needed given that it should already be enforced by the ruuvitag library
+
+    # This loop to check for authorized devices should not be needed given that it should already be enforced by the ruuvitag library
     is_authorized_device = False
     for sensor in authorized_devices:
         if mac == sensor[0]:
             is_authorized_device = True
-            logging.info(f"publishing message for {sensor[1]} ({mac}).")
-            client.publish(f"$iothub/{sensor[1]}/messages/events", str(payload), qos=1)
+            logging.info(
+                f"publishing message for {sensor[1]} [{mac}]: {str(payload)}.")
+            client.publish(
+                f"$iothub/{sensor[1]}/messages/events", str(payload), qos=1)
     if(is_authorized_device == False):
-        logging.warning(f"ignoring message from an unknown sensor (MAC address: {mac}).")
+        logging.warning(
+            f"ignoring message from an unknown sensor (MAC address: {mac}).")
 
 
 def main():
-    logging.info("Protocol Translation Module (PTM) for Python")
+    logging.info("Azure IoT Edge Protocol Translation Module (PTM) Sample")
 
     gateway_hostname = os.environ["IOTEDGE_GATEWAYHOSTNAME"]
 
@@ -59,7 +67,8 @@ def main():
 
     # listening for sensors data.
     logging.info("listening for sensors data.")
-    RuuviTagSensor.get_datas(callback, [sensor[0] for sensor in authorized_devices])
+    RuuviTagSensor.get_datas(callback, [sensor[0]
+                                        for sensor in authorized_devices])
 
     logging.info("exiting.")
 
